829bdb492679783cd840036329c79add8c3fe270
Alexander <alexandervolodin2020@gmail.com>
2023-12-15 07:46:40 +0300

    Fixed upload file design

--- silaeder-conference/src/pages/api/getProjectById.js
+++ silaeder-conference/src/pages/api/getProjectById.js
@@ -10,13 +10,22 @@ export default async function getProjectById(req, res) {
             users: true
         }
     })
+
     const projects = []
-    all_projects.forEach((project) => {
+    for (const project of all_projects) {
         // console.log(project.users[0].projectId, id, user_id)
         if ((project.users[0].projectId === id) && (project.users[0].userId === user_id)) {
+            const c_id = project.conferenceId
+            const conference = await prisma.conference.findFirst({
+                where: {
+                    id: c_id
+                }
+            })
+            project.conferenceId = { label: conference.name, value: conference.id }
+            console.log(project)
             projects.push(project)
         }
-    })
+    }
 
     await res.status(200).json({ project: projects[0] })
 }
